a87766fa7507b30f18cb4702ed4793ce71d01ce1
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-05-01 11:59:59 -0300

    chore: add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'movie-1985a',
+    appId: '1:246364947349:web:9773a312c4f840d5242c9c',
+    databaseURL: 'https://movie-1985a-default-rtdb.firebaseio.com',
+    storageBucket: 'movie-1985a.appspot.com',
+    apiKey: 'AIzaSyA4uScU2zBmDCNmLBpMARwabu2BBa2Kf24',
+    authDomain: 'movie-1985a.firebaseapp.com',
+    messagingSenderId: '246364947349',
+    measurementId: 'G-HCSJ81CXJ6',
+  },
   production: false
 };
 
